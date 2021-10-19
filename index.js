530f63ac8355bf76e8aa87d8c13394894570aa5d
GUCLU Sefa <sefaaguclu@gmail.com>
2021-10-19 11:43:27 +0200

    frontend changes

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
-let test = "hello world";
-let test2 = "gdfkgdfjgfdjgh";
+// inclure les dépendances et middlewares
+const mysql = require('mysql')
+const express = require('express')
+const ejs = require('ejs')
+const iniparser = require('iniparser')
+
+// activer les dépendances
+let configDB = iniparser.parseSync('config/DB.ini')
+let app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('assets'))
+app.use(express.static('views'))
+
+let mysqlconnexion = mysql.createConnection({
+    host: configDB['dev']['host'],
+    user: configDB['dev']['user'],
+    password: configDB['dev']['password'],
+    database: configDB['dev']['dbname']
+})
+mysqlconnexion.connect((err) => {
+    if (!err) console.log('BDD connectée.')
+    else console.log('BDD connexion échouée \n Erreur: ' + JSON.stringify(err))
+})
+
+app.listen(3000, () => console.log('le serveur Livre d\'Or est prêt.'))
+// utiliser les routesrs
+app.get('/', (req, res) => {
+    res.send('Le serveur sautheuhz est actif')
+})
+app.get('/sautheuhz', (req, res) => {
+    res.render('accueil')
+})
