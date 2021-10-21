7a6dc2d10e6eeabf6dcb0a440ed977691147b61e
Sefa GUCLU <sefaaguclu@gmail.com>
2021-10-21 22:22:39 +0200

    5

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -11,3 +11,16 @@
         }, false);
     }, false);
 })();
+
+$(document).ready(function() {  
+  
+    $(".add-more").click(function(){   
+        var html = $(".copy").html();  
+        $(".after-add-more").after(html);  
+    });  
+
+    $("body").on("click",".remove",function(){   
+        $(this).parents(".control-group").remove();  
+    });  
+
+  });  
