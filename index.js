57519d54d30c42d10fa2aaeb345ccbca01dc42bf
GUCLU Sefa <sefaaguclu@gmail.com>
2021-11-19 16:57:17 +0100

    index

--- index.js
+++ index.js
@@ -14,31 +14,15 @@ let app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('assets'))
 app.use(express.static('views'))
-
 app.use(session({
 	secret:'leCodeSecretFlash',
 	saveUninitialized: true,
 	resave: true
 }));
-
 app.use(flash());
-
-// connexion mysql
-let configDB = iniparser.parseSync('config/DB.ini')
-let mysqlconnexion = mysql.createConnection({
-    host: configDB['dev']['host'],
-    user: configDB['dev']['user'],
-    password: configDB['dev']['password'],
-    database: configDB['dev']['dbname']
-})
-mysqlconnexion.connect((err) => {
-    if (!err) console.log('BDD connectée.')
-    else console.log('BDD connexion échouée \n Erreur: ' + JSON.stringify(err))
-})
 
 app.listen(3000, () => console.log('Le serveur sautheuhz est actif'))
 app.get('/', (req, res) => {
     res.send('Le serveur sautheuhz est actif')
 })
-
 app.use('/sautheuhz', Routeur)
