0652b5f3b4b2344763ff6078b1efb1c107fc5089
GUCLU Sefa <sefaaguclu@gmail.com>
2021-11-19 16:54:31 +0100

    index

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 const mysql = require('mysql')
 const express = require('express')
 const ejs = require('ejs')
-const iniparser = require('iniparser')
-const Routeur = require('./routes/routes');
 const session = require('express-session');
 const flash = require('connect-flash');
+
+const iniparser = require('iniparser')
+const Routeur = require('./routes/routes');
 
 // activer les dépendances
 let app = express()
 app.set('view engine', 'ejs')
 app.use(express.static('assets'))
 app.use(express.static('views'))
-
 
 app.use(session({
 	secret:'leCodeSecretFlash',
