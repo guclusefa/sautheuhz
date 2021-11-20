646c8e41ffb3bfce4a47e9fe9a18d58fe12ac6f7
Sefa GUCLU <sefaaguclu@gmail.com>
2021-11-20 23:03:02 +0100

    stock fix

--- controllers/ctrlStock.js
+++ controllers/ctrlStock.js
@@ -2,18 +2,27 @@ var modelStock = require('../models/modelStock');
 module.exports = {
     afficher_liste_stocks: function (req, res) {
         modelStock.afficher_liste_stocks(function (data, data2, data3) {
+            console.log(data)
+            console.log("--------------------------------")
             console.log(data2)
             console.log("--------------------------------")
             console.log(data3)
+
+            ajd = new Date();
             lesStock = []
-            for(i in data2){
+            for (i in data2) {
                 lesStock.push([data2[i].idMedicament, data2[i].Medicaments_libelle, data2[i].Medicaments_qte, data2[i].stock_necessaire])
             }
-            for(i in data3){
+
+            for (i in data3) {
                 lesStock.push([data3[i].Medicaments_id, data3[i].Medicaments_libelle, data3[i].Medicaments_qte, 0])
             }
+
+            for (i in data) {
+                lesStock[i][3] = lesStock[i][3] - data[i].stock_necessaire
+            } 
             console.log(lesStock)
-            res.render('./liste_stocks', {lesStock, contenu: data, contenud: data2, titre: "Les stocks" })
+            res.render('./liste_stocks', { lesStock, contenu: data, contenud: data2, titre: "Les stocks" })
         });
     },
     afficher_form_stock: function (req, res) {
